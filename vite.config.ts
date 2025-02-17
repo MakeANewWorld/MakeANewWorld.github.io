1db9d196c689db689605888b68ebd4d989561e69
NOBTG <make.a.world.git@gmail.com>
2025-02-17 23:12:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [react()],
+  root:'./src/pages/html',
   build: {
     rollupOptions: {
       input: {
-        main: '/src/pages/html/index.html',
-        getStart: '/src/pages/html/get-start.html',
-        play: '/src/pages/html/play.html',
+        main: resolve(__dirname, 'src/pages/html/index.html'),
+        getStart: resolve(__dirname, 'src/pages/html/get-start.html'),
+        play: resolve(__dirname, 'src/pages/html/play.html'),
       },
     },
-    outDir: '../../../../dist'
   },
 })
