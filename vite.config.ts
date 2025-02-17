6d63c7e9733d92a1ed5bdc88e4f12d59d2ad5179
NOBTG <make.a.world.git@gmail.com>
2025-02-17 22:48:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'src/pages/html',
+  root: '/src/pages/html/',
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -11,11 +11,6 @@ export default defineConfig({
         main: 'index.html',
         getStart: 'get-start.html',
         play: 'play.html',
-      },
-      output: {
-        manualChunks: {
-          lodash: ['lodash']
-        }
       },
     },
   },
